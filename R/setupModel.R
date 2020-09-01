d1d316a7ee34e6d794ef1d7df58380c588dff083
Kevin Rosenfield <kar61@psu.edu>
2020-09-01 19:57:25 -0400

    setupModel update

--- R/setupModel.R
+++ R/setupModel.R
@@ -1,8 +1,9 @@
-setupABM <- function(Dimensions = 2) {
+setupABM <- function(dimensions = 2) {
   model <-  list(
     "numberAgents" = sample(1:100, 1),
     "Territorial" = sample(c(FALSE, TRUE), 1),
     "worldDiameterMeters" = runif(1, 25, 1000),
+    "dimensions" = dimensions,
     "dailyActivityProp" = abs(rnorm(1, 12, 2.5)))
   model <- c(model,
     if( Dimensions == 2) {
@@ -31,6 +32,6 @@ setupABM <- function(Dimensions = 2) {
     "sdFemaleMetersPerHour" = abs(runif(1, 0, model$meanFemaleMetersPerHour / 3)),
     "sdMaleMetersPerHour" = abs(runif(1, 0, model$meanMaleMetersPerHour / 3))
   )
-  model <-  model[-5]
+  model <-  model[-6]
   return(model)
 }
