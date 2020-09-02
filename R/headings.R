b03077038ab8a16a650067f9d698bb42c67a4dc0
Kevin Rosenfield <kar61@psu.edu>
2020-09-01 21:11:37 -0400

    fix headings.R

--- R/headings.R
+++ R/headings.R
@@ -1,17 +1,17 @@
 
 
-setHeading2D <- function(df = dfAgent, numberAgents) {
+setHeading2D <- function(df = dfAgent, numberAgents = dfABM$numberAgents) {
   df$Heading1 = df$Heading1 + rnorm(numberAgents, 0, 20)
   df$Heading1 = ifelse(df$Heading1 > 360 | df$Heading1 < 0, abs(abs(df$Heading1) - 360), df$Heading1)
-  return(df$Heading1)
+  return(df)
 }
 
-setHeading3D <- function(df = dfAgent, numberAgents) {
+setHeading3D <- function(df = dfAgent, numberAgents = dfABM$numberAgents) {
   df$Heading1 = df$Heading1 + rnorm(numberAgents, 0, 20)
   df$Heading2 = df$Heading2 + rnorm(numberAgents, 0, 20)
   df$Heading1 = ifelse(df$Heading1 > 360 | df$Heading1 < 0, abs(abs(df$Heading1) - 360), df$Heading1)
   df$Heading2 = ifelse(df$Heading2 > 360 | df$Heading2 < 0, abs(abs(df$Heading2) - 360), df$Heading2)
-  return(c(df$Heading1, df$Heading2))
+  return(df)
 }
 
 # turn around if agent has left the world
@@ -21,7 +21,7 @@ reverseHeading2D <- function(df = dfAgent) {
   return(df$Heading1)
 }
 
-reverseHeading3D <- function(df = dfAgent, i. = i) {
+reverseHeading3D <- function(df = dfAgent) {
   df$Heading1 = ifelse(df$Heading1 > 180, df$Heading1 - 180, df$Heading1 + 180)
   df$Heading2 = ifelse(df$Heading2 > 180, df$Heading2 - 180, df$Heading2 + 180)
   return(c(df$Heading1, df$Heading2))
