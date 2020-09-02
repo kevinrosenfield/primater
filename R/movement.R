45f00e3894e0b02a860caaef3d3d9619c1164d0f
Kevin Rosenfield <kar61@psu.edu>
2020-09-01 21:16:43 -0400

    fix movement.R

--- R/movement.R
+++ R/movement.R
@@ -1,12 +1,12 @@
 move2D <- function(df = dfAgent) {
   df$xCor = df$xCor + (df$metersPerHour * cos(df$Heading1))
   df$yCor = df$yCor + (df$metersPerHour * sin(df$Heading1))
-  return(c(df$xCor, df$yCor))
+  return(df)
 }
 
 move3D <- function(df = dfAgent) {
   df$xCor = df$xCor + (df$metersPerHour * cos(df$Heading2 * (pi / 180)) * sin(df$Heading1 * (pi / 180)))
   df$yCor = df$yCor + (df$metersPerHour * sin(df$Heading2 * (pi / 180)))
   df$zCor = df$zCor + (df$metersPerHour * cos(df$Heading2 * (pi / 180)) * cos(df$Heading1 * (pi / 180)))
-  return(c(df$xCor, df$yCor, df$zCor))
+  return(df)
   }
