72cc9c52f006bea1a13f6b957515c21adfccfddc
Kevin Rosenfield <kar61@psu.edu>
2020-09-14 17:24:13 -0400

    pointSize stuff

--- R/createAgents.R
+++ R/createAgents.R
@@ -40,7 +40,7 @@ setupAgents <- function(df = dfABM) {
 
   cexSizes <- list()
   cexConstant <- ifelse(dfABM$groupLiving == T, 20.1, 0.8)
-  cexConstant <- ifelse(Sys.info()[['sysname']] == 'Windows' == T, cexConstant * 2, cexConstant)
+  cexConstant <- ifelse((Sys.info()[['sysname']] == 'Windows') == T, cexConstant * 2, cexConstant)
 
   fig <<- plot(NA, xlim =  c(0 - dfABM$worldRadius, dfABM$worldRadius), ylim = c(0 - dfABM$worldRadius, dfABM$worldRadius))
   #rm(fig, pos=".GlobalEnv")
